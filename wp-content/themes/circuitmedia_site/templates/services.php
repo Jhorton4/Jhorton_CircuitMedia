0f86d5f755ba404b816609f867f736e28a9f4a33
Joseph Horton <joe.04hort@gmail.com>
2015-04-14 22:04:39 -0600

    04/14/2015 update

---
+++ wp-content/themes/circuitmedia_site/templates/services.php
@@ -0,0 +1,54 @@
+ <?php
+  /*
+  Template Name: Services
+  */
+
+get_header(); ?>
+
+<div class="hero" style="background-image: url(<?php the_field('hero_background_image'); ?>);">
+    <div class="content-wrap row">
+        <h3><?php the_field('services_headline'); ?></h3>
+        <hr/>
+    </div>
+</div>
+
+<div class="gallery">
+
+<?php
+    // The Arguments
+    $args = array(
+        'post_type' => 'abilities',  // Name of Your Custom Post Type
+        'posts_per_page' => 4       // Number of Posts to Retrieve
+    );
+    // Start Loop
+    $loop = new WP_Query( $args );
+    while ( $loop->have_posts() ) : $loop->the_post();
+?>
+
+
+<div class="column">
+    <div class="feature-image-overlay" style= "background-image: url('<?php the_field('ability_icon'); ?>');" >
+      <h3><?php the_title(); ?></h3>
+      
+      <p><?php the_field('ability_skill_1'); ?></p>
+      <p><?php the_field('ability_description_1'); ?></p>
+      
+      <p><?php the_field('ability_skill_2'); ?></p>
+      <p><?php the_field('ability_description_2'); ?></p>
+      
+      <p><?php the_field('ability_skill_3'); ?></p>
+      <p><?php the_field('ability_description_3'); ?></p>
+
+    </div>
+ </div>
+
+<?php
+    // Resets the Loop
+    endwhile;
+    wp_reset_postdata();
+?>
+ </div>
+<?php get_footer(); ?>
+
+
+
