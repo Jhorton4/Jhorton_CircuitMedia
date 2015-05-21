4dc5b1eec9b0dbb6aabb50cd1d57fd385fd3f33f
Joseph Horton <josephhorton@Macintosh.local>
2015-05-20 22:01:56 -0600

    5/20/2015
    Woocommerce and Work pages

--- wp-content/themes/circuitmedia_site/templates/work.php
+++ wp-content/themes/circuitmedia_site/templates/work.php
@@ -7,48 +7,42 @@ get_header(); ?>
 
 <div class="title">
     <div class="description row">
-        <h3><?php the_field('work_headline'); ?></h3>
+          <h3><?php the_field('work_headline'); ?></h3>
         <hr/>
     </div>
 </div>
 
-
 <div class="gallery">
 
-<?php
-    // The Arguments
-    $args = array(
-        'post_type' => 'works',  // Name of Your Custom Post Type
-        'posts_per_page' => 9       // Number of Posts to Retrieve
-    );
-    // Start Loop
-    $loop = new WP_Query( $args );
-    while ( $loop->have_posts() ) : $loop->the_post();
-?>
+    <?php
+        // The Arguments
+        $args = array(
+            'post_type' => 'work',  // Name of Your Custom Post Type
+            'posts_per_page' => 9       // Number of Posts to Retrieve
+        );
+        // Start Loop
+        $loop = new WP_Query( $args );
+        while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
+        <div class="column">
+            <div class="overlay">
+                <div class="feature-image-overlay" style= "background-image: url('<?php the_field('piece_feature_image'); ?>');">
+                    <div class="more-info">
 
-<div class="column">
-    <div class="feature-image-overlay" style= "background-image: url('<?php the_field('piece_feature_image'); ?>');" >
-      <h3><?php the_title(); ?></h3>
-    </div>
- </div>
+                        <h2><?php the_title(); ?></h2>
+                         <p><?php the_field('piece_title'); ?></p>
+                          <hr>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-<?php
-    // Resets the Loop
-    endwhile;
-    wp_reset_postdata();
-?>
+          <?php
+              // Resets the Loop
+              endwhile;
+              wp_reset_postdata(); ?>
 </div>
-
-<?php get_footer(); ?>
-
-
-
-<div class="column third">
-    <a class="feature-image-overlay" href="<?php the_permalink(); ?>"><img src="<?php the_field('portfolio_feature_image'); ?>" /></a>
-    <a href="<?php the_permalink(); ?>"<h3><?php the_title(); ?></h3></a>
-    <p><?php the_field('portfolio_short_description'); ?></p>
- </div>
+          <?php get_footer(); ?>
 
 
 
