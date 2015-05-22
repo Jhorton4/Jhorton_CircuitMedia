43003c6d85480d0fd88f155bcbe9ba8a34793238
Joseph Horton <josephhorton@Macintosh.local>
2015-05-22 13:13:32 -0600

    5/22/2015
    Transitions

--- wp-content/themes/circuitmedia_site/templates/meetus.php
+++ wp-content/themes/circuitmedia_site/templates/meetus.php
@@ -8,10 +8,10 @@ get_header(); ?>
 <div class="title">
     <div class="description row">
   		  <h3><?php the_field('team_headline_1'); ?></h3>
-        <hr/>
+        <hr>
   		  <p><?php the_field('team_description'); ?></p>
         <h3><?php the_field('team_headline_2'); ?></h3>
-        <hr/>
+        <hr>
     </div>
 </div>
 
@@ -36,8 +36,8 @@ get_header(); ?>
                         <h2><?php the_title(); ?></h2>
                          <p><?php the_field('person_title'); ?></p>
                           <hr>
-                          <a href= "mailto:<?php the_field('person_email_link'); ?>"><span class="genericon genericon-mail"></span></span></a>
-                          <a href="<?php the_field('person_linkedin_link'); ?>"><span class="genericon genericon-linkedin-alt"></span></a>
+                          <a href= "mailto:<?php the_field('person_email_link'); ?>" target="_blank"><span class="genericon genericon-mail"></span></a>
+                          <a href="<?php the_field('person_linkedin_link'); ?>" target="_blank"><span class="genericon genericon-linkedin-alt"></span></a>
                     </div>
                 </div>
             </div>
--- wp-content/themes/circuitmedia_site/templates/work.php
+++ wp-content/themes/circuitmedia_site/templates/work.php
@@ -7,31 +7,37 @@ get_header(); ?>
 
 <div class="title">
     <div class="description row">
-          <h3><?php the_field('work_headline'); ?></h3>
-        <hr/>
+        <h3><?php the_field('team_headline_1'); ?></h3>
+        <hr>
+        <p><?php the_field('team_description'); ?></p>
+        <h3><?php the_field('team_headline_2'); ?></h3>
+        <hr>
     </div>
 </div>
 
 <div class="gallery">
 
-    <?php
-        // The Arguments
-        $args = array(
-            'post_type' => 'work',  // Name of Your Custom Post Type
-            'posts_per_page' => 9       // Number of Posts to Retrieve
-        );
-        // Start Loop
-        $loop = new WP_Query( $args );
-        while ( $loop->have_posts() ) : $loop->the_post(); ?>
+<?php
+    // The Arguments
+    $args = array(
+        'post_type' => 'people',  // Name of Your Custom Post Type
+        'posts_per_page' => 12       // Number of Posts to Retrieve
+    );
+    // Start Loop
+    $loop = new WP_Query( $args );
+    while ( $loop->have_posts() ) : $loop->the_post();
+?>
 
         <div class="column">
             <div class="overlay">
-                <div class="feature-image-overlay" style= "background-image: url('<?php the_field('piece_feature_image'); ?>');">
+                <div class="feature-image-overlay" style= "background-image: url('<?php the_field('person_headshot'); ?>');">
                     <div class="more-info">
 
                         <h2><?php the_title(); ?></h2>
-                         <p><?php the_field('piece_title'); ?></p>
+                         <p><?php the_field('person_title'); ?></p>
                           <hr>
+                          <a href= "mailto:<?php the_field('person_email_link'); ?>" target="_blank"><span class="genericon genericon-mail"></span></a>
+                          <a href="<?php the_field('person_linkedin_link'); ?>" target="_blank"><span class="genericon genericon-linkedin-alt"></span></a>
                     </div>
                 </div>
             </div>
